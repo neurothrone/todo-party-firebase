e82d8bdcbb06250d9626d213dd0e0d3d563066df
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-24 14:27:17 +0200

    Initial Commit

---
+++ TodoParty/TodoPartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoPartyApp.swift
+//  TodoParty
+//
+//  Created by Zaid Neurothrone on 2022-10-24.
+//
+
+import SwiftUI
+
+@main
+struct TodoPartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
